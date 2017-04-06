52dddf0d34371ddc76024f890d51e3c57a115b76
minchulahn <minchulahn@inslab.co.kr>
2017-04-06 14:08:02 +0900

    Create sonarqube docker

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM ubuntu:14.04
+MAINTAINER Minchul Ahn <minchulahn@inslab.co.kr>
+
+# Install dependencies
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update
+RUN apt-get -y install unzip curl openjdk-7-jre-headless mysql-server-5.5 mysql-client
+RUN cd /tmp && curl -L -O https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-5.1.2.zip && unzip sonarqube-5.1.2.zip && mv sonarqube-5.1.2 /opt/sonar
+
+# Update SonarQube configuration
+#RUN sed -i 's|#wrapper.java.additional.7=-server|wrapper.java.additional.7=-server|g' /opt/sonar/conf/wrapper.conf
+RUN sed -i 's|#sonar.jdbc.username=sonar|sonar.jdbc.username=root|g' /opt/sonar/conf/sonar.properties
+RUN sed -i 's|#sonar.jdbc.password=sonar|sonar.jdbc.password=root|g' /opt/sonar/conf/sonar.properties
+#RUN sed -i 's|sonar.jdbc.url=jdbc:h2|#sonar.jdbc.url=jdbc:h2|g' /opt/sonar/conf/sonar.properties
+RUN sed -i 's|#sonar.jdbc.url=jdbc:mysql://localhost:3306/sonar|sonar.jdbc.url=jdbc:mysql://localhost:3306/sonar|g' /opt/sonar/conf/sonar.properties
+# Set context path
+RUN sed -i 's/#sonar.web.context=/sonar.web.context=\/sonarqube/g' /opt/sonar/conf/sonar.properties
+
+RUN mkdir -p /tmp/sonar
+RUN cp -rf /opt/sonar/extensions /tmp/sonar/
+RUN cp -rf /opt/sonar/conf /tmp/sonar/
+ADD start.sh /opt/sonar/bin/linux-x86-64/start.sh
+RUN chmod 777 /opt/sonar/bin/linux-x86-64/start.sh
+
+EXPOSE 9000
+
+VOLUME ["/opt/sonar/extensions", "/opt/sonar/conf"]
+VOLUME ["/var/lib/mysql"]
+
+CMD ["/opt/sonar/bin/linux-x86-64/start.sh"]
---
+++ start.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cat > /opt/sonar/bin/linux-x86-64/createdb.sql <<EOF
+    CREATE DATABASE sonar;
+    USE mysql;
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+    UPDATE user SET password=PASSWORD("root") WHERE user='root';
+    FLUSH PRIVILEGES;
+EOF
+
+mysql_install_db --user mysql > /dev/null
+mysqld --bootstrap --verbose=0 < /opt/sonar/bin/linux-x86-64/createdb.sql
+mysqld_safe --user=mysql &
+
+if [ ! -d "/opt/sonar/extensions/plugins" ]; then
+    echo "Copying plugins..."
+    cp -rf /tmp/sonar/extensions /opt/sonar/
+    rm -rf /tmp/sonar/extensions
+    cd /opt/sonar/extensions/plugins && \
+        curl -O http://downloads.sonarsource.com/plugins/org/codehaus/sonar-plugins/sonar-ldap-plugin/1.4/sonar-ldap-plugin-1.4.jar
+fi
+
+if [ ! -f "/opt/sonar/conf/sonar.properties" ]; then
+    echo "Copying configuration..."
+    cp -rf /tmp/sonar/conf /opt/sonar/
+fi
+
+if [ -n "$LDAP_URI" ] && [ -n "$LDAP_BASE_DN" ]
+then
+    echo "--> LDAP setting..."
+    CONF_FILE=/opt/sonar/conf/sonar.properties
+    sed -i '/^sonar.security.realm/d' $CONF_FILE
+    sed -i '/^sonar.security.savePassword/d' $CONF_FILE
+    sed -i '/^ldap.url/d' $CONF_FILE
+    sed -i '/^ldap.user/d' $CONF_FILE
+    sed -i '/^ldap.group/d' $CONF_FILE
+    echo -e "\n\n" >> $CONF_FILE
+    echo "sonar.security.realm=LDAP" >> $CONF_FILE
+    echo "sonar.security.savePassword=false" >> $CONF_FILE
+    echo "ldap.url=$LDAP_URI" >> $CONF_FILE
+    echo "ldap.user.baseDn=$LDAP_BASE_DN" >> $CONF_FILE
+    echo "ldap.user.request=(&(objectClass=PosixAccount)(uid={login}))" >> $CONF_FILE
+    echo "ldap.user.realNameAttribute=cn" >> $CONF_FILE
+    echo "ldap.user.emailAttribute=mail" >> $CONF_FILE
+else
+    echo "Skip LDAP setting..."
+fi
+
+/opt/sonar/bin/linux-x86-64/sonar.sh console
